5f8bfac1ed2a5fdd560bddad6695f6a0a645ec48
edmond akwasi <edmondakwasi133@gmail.com>
2025-02-10 12:01:44 +0000

    added a client component

--- EPCPR/src/App.jsx
+++ EPCPR/src/App.jsx
@@ -1,29 +1,18 @@
 import { useState } from 'react'
 
 import './App.css'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
 
 function App() {
   
 
   return (
-
-    <div className="hero bg-base-200 min-h-screen">
-    <div className="hero-content flex-col lg:flex-row-reverse">
-      <img
-        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-        className="max-w-sm rounded-lg shadow-2xl" />
-      <div>
-        <h1 className="text-5xl font-bold">Box Office News!</h1>
-        <p className="py-6">
-          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-          quasi. In deleniti eaque aut repudiandae et a id nisi.
-        </p>
-        <button className="btn btn-primary">Get Started</button>
-      </div>
-    </div>
-  </div>
-  
-  )
-}
+    <>
+    <Navbar />
+    <Hero />
+    </>
+  );
+};
 
 export default App
