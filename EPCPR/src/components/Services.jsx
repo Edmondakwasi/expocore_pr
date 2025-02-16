3b9bd83d4f2993adb64021e60d3cc5c2a411835d
edmond akwasi <edmondakwasi133@gmail.com>
2025-02-16 23:29:12 +0000

    added a description for the first card

--- EPCPR/src/components/Services.jsx
+++ EPCPR/src/components/Services.jsx
@@ -10,7 +10,7 @@ const Services = () => {
             <div className="card bg-primary text-primary-content">
               <div className="card-body p-4 md:p-6">
                 <h2 className="font-sans card-title text-lg md:text-xl">Global PR agency</h2>
-                <p className="font-sans text-sm md:text-base">If a dog chews shoes whose shoes does he choose?</p>
+                <p className="font-sans text-sm md:text-base">We specialize in global PR for big brands.</p>
               </div>
             </div>
 
