e9f7496161fb6368e0b93de0f19ea71cc5269d8b
edmond akwasi <edmondakwasi133@gmail.com>
2025-02-17 21:41:03 +0000

    positioned the navbar image to align with the page

--- EPCPR/src/components/Navbar.jsx
+++ EPCPR/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   return (
     <div className="navbar bg-base-100 relative">
-      <div className="flex-1">
+      <div className="flex-1 ml-5">
         <a href="/">
           <img src={Logo} alt="logo" className="w-32 h-16 p-2 md:w-40 md:h-20 md:ml-24" />
         </a>
