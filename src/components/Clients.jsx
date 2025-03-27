c927b56322a5706a991ebc033926042c32a96cbe
edmond akwasi <edmondakwasi133@gmail.com>
2025-03-27 17:15:17 +0000

    changed the brand logo

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+import brand1Image from '../assets/brand1.png'
+import brand2Image from '../assets/brands.png'
+
+
 const Clients = () => {
   return (
     <div className="container mx-auto px-4 md:px-8">
@@ -10,13 +14,13 @@ const Clients = () => {
       <div className="max-w-7xl mx-auto overflow-x-auto">
         <div className="flex flex-row justify-center items-center gap-4 md:gap-10 lg:gap-12 py-8 md:py-14 min-w-max px-4"> 
           <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            src={brand2Image}
             alt="Client 1"
             className="rounded-full w-24 h-24 md:w-40 md:h-40 lg:w-48 lg:h-48 object-cover"
           />
           
           <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            src={brand1Image} 
             alt="Client 2"
             className="rounded-full w-24 h-24 md:w-40 md:h-40 lg:w-48 lg:h-48 object-cover"
           />
