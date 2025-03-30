9b6fd844e6d8f470d747413435504a566bb37824
edmond akwasi <edmondakwasi133@gmail.com>
2025-03-30 13:07:58 +0000

    updated the footer services

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,16 +6,16 @@ const Footer = () => {
     <footer className="footer bg-base-200 text-base-content p-10">
   <nav>
     <h6 className="footer-title">Services</h6>
-    <a className="link link-hover">Branding</a>
-    <a className="link link-hover">Marketing</a>
-    <a className="link link-hover">Advertisement</a>
+    <a className="link link-hover">Brand Promotion</a>
+    <a className="link link-hover">Event Coordination</a>
+    <a className="link link-hover">Social Media Management</a>
   </nav>
   <nav>
     <h6 className="footer-title">Company</h6>
     <a className="link link-hover">About us</a>
     <a className="link link-hover">Contact</a>
   </nav>
-  <nav>
+  <nav>a
     <h6 className="footer-title">Legal</h6>
     <a className="link link-hover">Terms of use</a>
     <a className="link link-hover">Privacy policy</a>
